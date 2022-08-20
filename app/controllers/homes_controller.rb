dcade64c19040d3c491bced1a5bc9fd6b3eac492
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-08-20 05:20:03 +0000

    [Add]4-5編集機能作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,19 +1,19 @@
 class HomesController < ApplicationController
   def new
-     @list = List.new 
+     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
-   redirect_to list_path(list)
+   redirect_to list_path(list.id)
+  end
+
+  def edit
+    @list = List.find(params[:id])
   end
   
-
-  
   private
- 
- 
   def list_params
     params.require(:list).permit(:title, :body)
   end
